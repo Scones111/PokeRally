eb277b6dc582f57915796699fc66a248c4ade4e8
Scones111 <jonas.lykke.kjeldsen@gmail.com>
2022-05-02 17:28:42 +0200

    refactored

---
+++ tidy/RobotRallyTest.java
@@ -0,0 +1,12 @@
+package tidy;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/", publish = true)
+
+public class RobotRallyTest {
+
+}
